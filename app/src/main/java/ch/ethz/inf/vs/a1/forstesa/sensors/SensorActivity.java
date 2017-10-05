02aec5cfe581bee22ce36d2acccff1328a216010
Sascha <sascha.forster@hotmail.ch>
2017-10-05 18:49:19 +0200

    work in progress

--- app/src/main/java/ch/ethz/inf/vs/a1/forstesa/sensors/SensorActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/forstesa/sensors/SensorActivity.java
@@ -12,7 +12,17 @@ import java.util.List;
 
 import static ch.ethz.inf.vs.a1.forstesa.sensors.MainActivity.sens_man;
 
-public class SensorActivity extends AppCompatActivity {
+public class SensorActivity extends AppCompatActivity implements SensorEventListener{
+
+    protected void onResume() {
+        super.onResume();
+        sens_man.registerListener(this, sens, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sens_man.unregisterListener(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,30 +30,40 @@ public class SensorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sensor);
 
         sens_name = getIntent().getExtras().getString("sensor_name");
+        //System.out.println("DEBUG: sens_name"+sens_name);
         List<Sensor> sens_list = sens_man.getSensorList(Sensor.TYPE_ALL);
         for(Sensor i : sens_list){
-            if(i.getName() == sens_name) sens = i;
+            if(i.getName().equals(sens_name)) sens = i;
+            //System.out.println("DEBUG: i.name="+i.getName());
         }
+        //System.out.println("DEBUG: "+sens);
         text = (TextView) findViewById(R.id.text_view);
         text.setText(sens_name + "\n");
+
+        sens_man.registerListener(this, sens, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    private class MySensorEventListener implements SensorEventListener {
-
-        @Override
-        public void onSensorChanged(SensorEvent sensorEvent) {
-            for (int k = 0; k < SensorTypesImpl.getNumberValues(sensorEvent.sensor.getType()));
-            text.append(sensorEvent.);
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        //System.out.println("DEBUG: onSensorChanged");
+        SensorTypesImpl sti = new SensorTypesImpl();
+        float[] vals = sensorEvent.values.clone();
+        //System.out.println("DEBUG: vals=" + vals);
+        String str = "";
+        for (int k = 0; k < sti.getNumberValues(sensorEvent.sensor.getType()); k++){
+            str.concat("SensorValue" + k + vals[k] + "\n");
+            //System.out.println("DEBUG: k=" + k);
         }
+        text.setText(sens_name + "\n" + str);
+    }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int i) {
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
-        }
     }
 
     private TextView text;
     private String sens_name;
-    private MySensorEventListener msel = new MySensorEventListener();
+    //private MySensorEventListener msel = new MySensorEventListener();
     private Sensor sens;
 }
--- app/src/main/java/ch/ethz/inf/vs/a1/forstesa/sensors/SensorTypesImpl.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/forstesa/sensors/SensorTypesImpl.java
@@ -46,6 +46,22 @@ public class SensorTypesImpl implements SensorTypes {
         switch (sensorType) {
             case 1:
                 return 3;
+            case 2:
+                return 3;
+            case 4:
+                return 3;
+            case 5:
+                return 1;
+            case 6:
+                return 1;
+            case 9:
+                return 3;
+            case 10:
+                return 3;
+            case 11:
+                return 3;
+            case 12:
+                return 1;
             case 13:
                 return 1;
             default:
@@ -54,6 +70,29 @@ public class SensorTypesImpl implements SensorTypes {
     }
 
     public String getUnitString(int sensorType){
-        return "hello";
+        switch (sensorType){
+            case 1:
+                return "m/s^2";
+            case 2:
+                return "microT";
+            case 4:
+                return "rad/s";
+            case 5:
+                return "lx";
+            case 6:
+                return "hPa";
+            case 9:
+                return "m/s^2";
+            case 10:
+                return "m/s^2";
+            case 11:
+                return "no unit";
+            case 12:
+                return "%";
+            case 13:
+                return "°C";
+            default:
+                return "no unit";
+        }
     }
 }
